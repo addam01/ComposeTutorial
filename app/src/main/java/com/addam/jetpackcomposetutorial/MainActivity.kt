529fc6af1b0498e395167d7b5305f2ed8bd398db
Addam <addam01@hotmail.com>
2022-11-22 21:51:35 +0800

    Pointed MainActivity.kt as main with navigation

--- app/src/main/java/com/addam/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/addam/jetpackcomposetutorial/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.addam.jetpackcomposetutorial
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.addam.jetpackcomposetutorial.features.login.LoginActivity
+import com.addam.jetpackcomposetutorial.features.posts.PostsActivity
 import com.addam.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    NavigationButtons()
                 }
             }
         }
@@ -30,14 +37,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+private fun NavigationButtons(){
+    val context = LocalContext.current
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ){
+        Button(
+            onClick = {
+                      context.startActivity(Intent(context, LoginActivity::class.java))
+            },
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth()
+        ){ Text(stringResource(R.string.login_form)) }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetpackComposeTutorialTheme {
-        Greeting("Android")
+        Spacer(modifier = Modifier.padding(16.dp))
+
+        Button(
+            onClick = {
+                context.startActivity(Intent(context, PostsActivity::class.java))
+            },
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth()
+        ){ Text(stringResource(R.string.post_api)) }
     }
 }
